14e141cca21cd3a4f8a26cb0a94f7bebb24f8d5f
NeaBouli <bergamolia@protonmail.com>
2025-09-18 22:23:02 +0200

    feat(contracts): add Boilerplate InfernoToken.sol + Teststub

---
+++ languages/solidity/contracts/InfernoToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// ---------------------------------------------------------
+// InfernoToken.sol (Boilerplate)
+// ---------------------------------------------------------
+// Platzhalter für den ERC20-basierten Inferno Token
+// Enthält noch keine Logik, wird im Coding-Chat generiert
+// ---------------------------------------------------------
+
+contract InfernoToken {
+    // TODO: Implementierung folgt
+}
---
+++ languages/solidity/tests/InfernoToken.test.js
@@ -0,0 +1,11 @@
+// InfernoToken.test.js (Boilerplate)
+// -----------------------------------
+// Platzhalter für Tests (z. B. mit Hardhat/Foundry)
+// Enthält noch keine Logik, wird im Coding-Chat generiert
+// -----------------------------------
+
+describe("InfernoToken", function () {
+  it("should deploy", async function () {
+    // TODO: Deployment-Test
+  });
+});
